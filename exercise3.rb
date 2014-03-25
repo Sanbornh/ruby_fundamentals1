29cd5aa5032c45b5915a7503f41d540ecc4f5c95
Sanborn <sanbornh@rogers.com>
2014-03-25 13:17:22 -0400

    added a bunch of functionality

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,30 @@ puts "What is your name? "
 name = gets.chomp
 
 puts "Hi #{name}!"
+
+while true
+	puts "How old are you #{name}?"
+	age = gets.chomp.to_i
+
+	if (age < 3) || (age > 90)
+		puts "That seems unlikely..."
+	else 
+		break
+	end
+end
+
+time = Time.new
+puts "You were born in #{time.year - age}"
+
+while true
+	puts "Is this correct? (y/n)"
+	correct = gets.chomp.upcase
+
+	if correct == "Y"
+		puts "Awesome!"
+		break
+	elsif correct == "N"
+		puts "Sorry about that. You must have been born in #{time.year - age - 1}"
+		break
+	end
+end
