b44cf84f5426dd90ce2434d9baea8c983e035c1d
Sanborn <sanbornh@rogers.com>
2014-03-25 12:38:24 -0400

    exercise 2 commit

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# This is a really good tip
+puts 55 * 1.25
+
+# Add an int to string
+puts 5.to_s + " is how many fingers I have."
+
+# String interpolation test
+puts "7839 * 45628 is equal to #{7839 * 45628}"
+
+# This will evaluate to true
+puts (true && false) || (false && false) || !(false && false)
